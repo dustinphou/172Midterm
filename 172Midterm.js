ae2c2720b0e2e738966a780b1af4847fa98e96e0
dustinphou <dustinphou@sjsu.edu>
2016-10-22 19:31:40 -0700

    Added date append to orders and csv export.

--- 172Midterm.js
+++ 172Midterm.js
@@ -1,8 +1,36 @@
-var csv = require('csv')
+var csv = require('fast-csv')
 var request = require('request')
 var repl = require('repl')
 var uu = require('underscore')
-var order_buffer = []
+var path = require('path')
+var fs = require('fs')
+var orderBuffer = []
+var filePath = path.join(__dirname, 'orders.csv')
+
+var weekday = new Array(7);
+weekday[0] = "Sun";
+weekday[1] = "Mon";
+weekday[2] = "Tue";
+weekday[3] = "Wed";
+weekday[4] = "Thu";
+weekday[5] = "Fri";
+weekday[6] = "Sat";
+
+var month = new Array(12);
+month[0] = "Jan";
+month[1] = "Feb";
+month[2] = "Mar";
+month[3] = "Apr";
+month[4] = "May";
+month[5] = "Jun";
+month[6] = "Jul";
+month[7] = "Aug";
+month[8] = "Sep";
+month[9] = "Oct";
+month[10] = "Nov";
+month[11] = "Dec";
+
+
 
 /*
 Take user input
@@ -17,104 +45,126 @@ Take user input
 ->save orders
 */
 
-function entry(cmd)
+function entry(cmd) //assumptions: all inputs will be given in ALL CAPS
 {
 	cmd = cmd.replace(/\n$/, '') //chops off /n added from REPL
-	var args = cmd.split(" ") // [action, amount, symb]
-	if(args[0] == "ORDERS")
+	var args = cmd.split(' ') // [action, amount, symb]
+	if(args[0] == 'ORDERS')
 	{
 		save_orders();
 	}
 	else
 	{
-		validation(args[0], args[1], args[2])
+		validation(args[0], args[1], args[2], cmd)
 	}
 }
 
-function validation(action, amount, symb)
+function validation(action, amount, symb, order)
 {
-	var order = {
-		"command": action,
-		"amount": amount,
-		"currency": symb
-	}
-	console.log(order)
 	if(!isNaN(amount) && amount > 0) //checks if a valid amount was specified
 	{
 		if(symb !== undefined)
 		{
-			var SYMB = symb.toUpperCase()
 			request('https://api.coinbase.com/v1/currencies', function (error, response, body){
 				if (!error && response.statusCode == 200) {
 					var currencies = JSON.parse(body)
 					currencies = uu.flatten(currencies)
-					if(uu.find(currencies, function (curr) {return curr == SYMB}))
+					if(uu.find(currencies, function (curr) {return curr == symb}))
 					{
-					 	console.log("Currency valid.")
-					 	transaction(action, amount, symb)
+					 	console.log('Currency valid.')
+					 	transaction(action, amount, symb, order)
 					} 
 					else
 					{
-						console.log("No known exchange rate for BTC/" + SYMB + ". Order failed.")				
+						console.log('No known exchange rate for BTC/' + symb + '. Order failed.')				
 					}
 				}
 				else
 				{
-					console.log("There was an error with the request.")
+					console.log('There was an error with the request.')
 				}
 			})
 		}
 		else
 		{
-			if(action == "BUY") 
+			order = order + ' BTC'
+			if(action == 'BUY') 
 			{
-				console.log("Order to BUY " + amount + " BTC queued.")
-				order_buffer.push(order)
+				console.log('Order to BUY ' + amount + ' BTC queued.')
+				orderBuffer.push(append_date_time(order) + ' : undefined')
 			}
-			else if (action == "SELL") 
+			else if (action == 'SELL') 
 			{
-				console.log("Order to SELL " + amount + " BTC queued.")
-				order_buffer.push(order)
+				console.log('Order to SELL ' + amount + ' BTC queued.')
+				orderBuffer.push(append_date_time(order) + ' : undefined')
 			}
-			else console.log("Invalid action.")
+			else console.log('Invalid action.')
 		}
 	}
 	else
 	{
-		console.log("No amount specified.")
+		console.log('No amount specified.')
 	}
 }
 
-function transaction(action, amount, symb)
+function transaction(action, amount, symb, order)
 {
-	var buy = 'btc_to_' + symb 	//create two strings, one to find buy rate and one for sell rate
-	var sell = symb + '_to_btc'
+	var low_symb = symb.toLowerCase()
+	var buy = 'btc_to_' + low_symb 	//create two strings, one to find buy rate and one for sell rate
+	var sell = low_symb + '_to_btc'
 	request('https://api.coinbase.com/v1/currencies/exchange_rates', function (error, response, body){
 		if (!error && response.statusCode == 200) {
 			var exchange_rates = JSON.parse(body)
 			if(action == 'BUY')
 			{
-				console.log("order to BUY " + amount + " worth of BTC queued @ " + exchange_rates[buy] + ' BTC/' + symb.toUpperCase() + ' (' + (exchange_rates[buy]*amount) + ' BTC)')
+				console.log('order to BUY ' + amount + ' worth of BTC queued @ ' + exchange_rates[buy] + ' BTC/' + symb.toUpperCase() + ' (' + (exchange_rates[buy]*amount) + ' BTC)')
+				orderBuffer.push(append_date_time(order) + ' : ' + exchange_rates[buy])
 			}
 			else if(action == 'SELL')
 			{
-				console.log("order to SELL " + amount + " worth of BTC queued @ " + exchange_rates[sell] + ' ' + symb.toUpperCase() + '/BTC' + ' (' + (exchange_rates[sell]*amount) + ' BTC)')
+				console.log('order to SELL ' + amount + ' worth of ' + symb + ' queued @ ' + exchange_rates[sell] + ' ' + symb.toUpperCase() + '/BTC' + ' (' + (exchange_rates[sell]*amount) + ' BTC)')
+				orderBuffer.push(append_date_time(order) + ' : ' + exchange_rates[sell])
 			}
 			else
 			{
-				console.log("Invalid action.")
+				console.log('Invalid action.')
 			}
+
 		}
 		else
 		{
-			console.log("There was an error with the request.")
+			console.log('There was an error with the request.')
 		}
 	})
 }
 
-function save_orders()
+function append_date_time(order)
 {
-	console.log(order_buffer)
+	//Wed Oct 05 2016 22:09:40 GMT+0000 (UTC) : BUY 10 : UNFILLED
+	//Weekday Month Day Year Time Timezone : order : UNFILLED
+	var d = new Date();
+	order = weekday[d.getUTCDay()] + ' ' + month[d.getUTCMonth()] + ' ' + d.getUTCDate() + ' ' + d.getUTCFullYear() + ' ' + d.getUTCHours() + ':' + d.getUTCMinutes() + ':' + d.getUTCSeconds() + ' : ' + order
+	return order
 }
 
-repl.start({prompt: "coinbase>", eval:entry})
+function save_orders()
+{
+	var csvBuffer = uu.map(orderBuffer, function (order) {
+		var splitorder = order.split(' : ')
+		var splitcmd = splitorder[1].split(' ')
+		var csvorder = {
+		"timestamp":splitorder[0],
+		"command":splitcmd[0],
+		"amount":splitcmd[1],
+		"currency":splitcmd[2],
+		"exchange rate to BTC": splitorder[2]
+		}
+		return csvorder
+	})
+
+var writableStream = fs.createWriteStream(filePath)
+csv.write(csvBuffer, {headers:true}).pipe(writableStream)
+
+}
+
+repl.start({prompt: 'coinbase>', eval:entry})
